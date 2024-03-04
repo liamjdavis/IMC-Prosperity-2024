af61bbb4061d115ef57d943270b24a0e99c4e174
Liam <davisliam123@gmail.com>
2024-03-04 10:29:08 -0500

    wrote basic tutorial round algo

---
+++ TutorialTrader.py
@@ -0,0 +1,63 @@
+from typing import Dict, List
+from datamodel import OrderDepth, TradingState, Order
+
+import collections
+from collections import defaultdict
+
+import random
+import math
+import copy
+
+import pandas as pd
+import numpy as np
+
+'''
+This is just copied and pasted from Prosperity Wiki so I can submit something and get the tutorial csv
+'''
+
+class Trader:
+    def run(self, state: TradingState):
+        # orders to be placed
+        result = {}
+
+        for product in state.order_depths:
+            order_depth: OrderDepth = state.order_depths[product]
+            # Initialize the list of Orders to be sent as an empty list
+            orders: List[Order] = []
+
+            # Define a fair value for the PRODUCT. Might be different for each tradable item
+            acceptable_price = 100
+
+			# All print statements output will be delivered inside test results
+            print("Acceptable price : " + str(acceptable_price))
+            print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
+            
+            # Order depth list come already sorted. 
+						# We can simply pick first item to check first item to get best bid or offer
+            if len(order_depth.sell_orders) != 0:
+                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+                if int(best_ask) < acceptable_price:
+                    # In case the lowest ask is lower than our fair value,
+                    # This presents an opportunity for us to buy cheaply
+                    # The code below therefore sends a BUY order at the price level of the ask,
+                    # with the same quantity
+                    # We expect this order to trade with the sell order
+                    print("BUY", str(-best_ask_amount) + "x", best_ask)
+                    orders.append(Order(product, best_ask, -best_ask_amount))
+    
+            if len(order_depth.buy_orders) != 0:
+                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+                if int(best_bid) > acceptable_price:
+										# Similar situation with sell orders
+                    print("SELL", str(best_bid_amount) + "x", best_bid)
+                    orders.append(Order(product, best_bid, -best_bid_amount))
+                
+            result[product] = orders
+    
+		# String value holding Trader state data required. 
+		# It will be delivered as TradingState.traderData on next execution.
+        traderData = "SAMPLE" 
+        
+		# Sample conversion request. Check more details below. 
+        conversions = 1
+        return result, conversions, traderData
